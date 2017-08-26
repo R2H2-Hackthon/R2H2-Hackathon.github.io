5d631c9736f0cdc7c6dedf6f3aee6943105a7224
Henrique Jensen <henriquejensen@hotmail.com>
2017-08-26 19:28:31 -0300

    troca para history no routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -8,8 +8,8 @@ import registerServiceWorker from './registerServiceWorker';
 import App from "./containers/App"
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
     , document.getElementById('root'));
 registerServiceWorker();
